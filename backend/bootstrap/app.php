9afb9573050f0b42c5ab5c5ab6fc4c4b91737471
elfjane.liu <elfjane.liu@piapp.com.tw>
2026-01-21 11:12:53 +0800

    新增功能

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -12,6 +12,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             'check.role' => \App\Http\Middleware\CheckRole::class,
         ]);
